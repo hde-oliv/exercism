39cd555727041429456dc8d2ac0c09ce815e3257
Henrique Rocha <henriquerocha@tutanota.com>
2021-09-19 00:38:21 -0400

    feat: acronym progress

--- crystal/acronym/src/acronym.cr
+++ crystal/acronym/src/acronym.cr
@@ -1,1 +1,12 @@
 # Please implement your solution to acronym in this file
+
+class Acronym
+  def Acronym.abbreviate(title : String)
+    first_letter_array = title.scan(/\b[a-zA-z]{1}/)
+    acronym = ""
+    first_letter_array.each do |letter|
+      acronym = acronym + letter[0]
+    end
+    return acronym.upcase
+  end
+end
