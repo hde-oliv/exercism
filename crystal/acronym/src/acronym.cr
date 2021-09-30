ac1aed899c6de7bac1d33fae08ea6e5dc1566a9d
Henrique Rocha <henriquerocha@tutanota.com>
2021-09-29 23:47:21 -0400

    feat: conclude acronym

--- crystal/acronym/src/acronym.cr
+++ crystal/acronym/src/acronym.cr
@@ -2,11 +2,11 @@
 
 class Acronym
   def Acronym.abbreviate(title : String)
-    first_letter_array = title.scan(/\b[a-zA-z]{1}/)
+    first_letter_array = title.scan(/(?<=\ |_|-|^)[A-Za-z]{1}/)
     acronym = ""
     first_letter_array.each do |letter|
       acronym = acronym + letter[0]
     end
-    return acronym.upcase
+    acronym.upcase
   end
 end
