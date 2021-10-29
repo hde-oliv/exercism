d1cbc5109a30859af025d860765746c4435088db
Henrique Rocha <henriquerocha@tutanota.com>
2021-10-28 22:48:54 -0300

    refactor: changes to python lasagna

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -1,11 +1,11 @@
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
+"""
+Lasagna
+"""
+
 EXPECTED_BAKE_TIME = 40
 
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
 PREPARATION_TIME = 2
 
-# TODO: define the 'bake_time_remaining()' function
 def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
@@ -19,9 +19,6 @@ def bake_time_remaining(elapsed_bake_time):
 
     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
-# TODO: define the 'preparation_time_in_minutes()' function
-#       and consider using 'PREPARATION_TIME' here
-
 def preparation_time_in_minutes(number_of_layers):
     """Calculate the preparation time based on the number of layers
 
@@ -33,13 +30,11 @@ def preparation_time_in_minutes(number_of_layers):
     """
     return PREPARATION_TIME * number_of_layers
 
-# TODO: define the 'elapsed_time_in_minutes()' function
-
 def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     """
     Return elapsed cooking time.
 
-    This function takes two numbers representing the number of layers & the time already spent 
+    This function takes two numbers representing the number of layers & the time already spent
     baking and calculates the total elapsed minutes spent cooking the lasagna.
     """
     return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
